40d0ba9f78c2dcee0b03e7876dd330e9f871cf91
echopchy <97993251+Perseids3@users.noreply.github.com>
2022-09-21 12:50:16 -0400

    Add files via upload

--- W2/ECE421_Assignment_1.py
+++ W2/ECE421_Assignment_1.py
@@ -1,61 +1,95 @@
-from unittest import skip
+
 from sklearn.datasets import load_breast_cancer
 import numpy as np
+import matplotlib.pyplot as plt
+
+### Problem 1.1
+### For the purpose of Problem 1.3, the return value of k_means algorithm includes the distortion, 
+### which is not technically the requirement of 1.1.
+
 data = load_breast_cancer()
-# Test 
-# print(data.target[[1,2,3,4,5,6,7,8,9,20,21]]) 569
-# print(type(data.target)) : numpy.ndarray
-
-X, Y = load_breast_cancer(return_X_y=True)
-
-
-### Helper function: calculate distance between two points
-def distance(a,b):
-    return np.sqrt((a[0]-b[0])**2+(a[1]-b[1])**2)
-### Test on helper function:
-# print(distance([6,7],[3,3]))
-
-### Helper function: Cost Function
-def cost_function(a,b):
-    res = 0
-    for i in range(len(a)):
-        res += distance(a[i],b)**2
-        
-    res = res/len(a)
-    #print(res)
-    return res
-
-
-
-def k_means(X_set, Y_set, k):
-    
+         
+def k_means(X, k):
     ### Random initialization of the centriods
     collection_set = [[] for i in range(k)]
-    centroids_set = [[None for i in range(2)] for i in range(k)]
+    centroids_set = [[] for i in range(k)]
     for i in range(k):
-    # Pick raw data points as the initial centroids
-        centroids_set[i][0] = X[i][0]
-        centroids_set[i][1] = Y[i]
-    # print(centroids_set)
+        random_index = np.random.randint(0, 569)
+        
+        centroids_set[i] = X[random_index]
+ 
+    
+    ### Initialize end condition
+    J_old = 0
+    J_new = 0
+    end = True
+    ### Start optimization
+    while end:
     ### Start classification
-    for i in range(len(X_set)):
-        for j in range(len(X_set[i])):
-           a = [X_set[i][j],Y_set[i]] 
-           d = 100000 ### A random large number to start iteration.
-           for m in range(k):
-                b = centroids_set[m]
-                if d > distance(a,b):
-                    #print(d)
-                    d = distance(a,b)
+        for i in range(len(X)):
+            d = 1000000 ### A random large number to start iteration.
+            for m in range(k):
+                if d > np.linalg.norm(np.array(X[i])-np.array(centroids_set[m])):
+                    d = np.linalg.norm(np.array(X[i])-np.array(centroids_set[m]))
                     pointer = m
                     
-           collection_set[pointer].append(a)
-    # print(len(collection_set[1]))
+            collection_set[pointer].append(X[i])
+        # print(len(collection_set[0]),len(collection_set[1]))
     
-    ### Now the raw data points are classified based on our original centroids
-    cost_function(collection_set[1], centroids_set[1])
-    ### Next we calculate the mean
-    
-    return None
+        ### Now the raw data points are classified based on our original centroids
+        ### We start to calculate and compare the distorsion 
+        
+        J_old = J_new ### Memorize the original distorsion value
+        J_new = 0
+        
+        ### We sum up distorsion for all clusters
+        for i in range(len(collection_set)):
+            for j in range(len(collection_set[i])):
+            # print("Cost function: ", cost_function(collection_set[i], centroids_set[i]) )
+                J_new += np.linalg.norm(np.array(collection_set[i][j])-np.array(centroids_set[i]))
+                
+        # print("This is J_old: ", J_old)
+        # print("This is J_new: ", J_new)
+        
+        if J_old != 0:
+            if abs((J_old-J_new)/J_old) < 0.005: ### If the distorsion value stops changing rapidly
+                # print("good")
+                end = False ### Time to end the loop
+                
+            else:
+                # print(abs((J_old-J_new)/J_old))
+                end = True
+        
+        ### Update the centroids by calculating mean
+        for i in range(k):
+            centroids_set[i] = np.mean(collection_set[i], axis=0)
+            # print("This is new centroids: ", centroids_set[i])
+        
+    return centroids_set, collection_set, J_new
 
-k_means(X, Y, 2)
+
+### Problem 1.2
+
+# for i in range(2,8):
+#     A, B, C = k_means(data.data, i)
+
+# The two inputs I passed into k-mean algorithm is the expected value of k (number of clusters)
+# as well as an array with 569 sub-arrays. Each sub-array contains 30 elements (dimensions). 
+
+
+
+# Problem 1.3
+res = []
+for i in range(2,8):
+    A, B, C = k_means(data.data, i)
+    res.append(C)
+    print("The distortion when i =", i, "is: ", C)
+    
+plt.plot([2, 3, 4, 5, 6, 7], res)
+plt.show()
+
+
+# Problem 1.4
+# After run the code multiple times, I found that the inflection point
+# usually occurs at k = 5. As result, I will choose k = 5 considering the accuracy of the result
+# as well as the efficiency (run time) of the code.
